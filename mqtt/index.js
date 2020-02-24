f8afb355f5b67927385f8074ff74f71014975397
Nikolaj Schlüter <nikolaj@bitkompagniet.dk>
2020-02-24 14:31:42 +0100

    add realtime data listener

--- mqtt/index.js
+++ mqtt/index.js
@@ -1,36 +1,41 @@
 
-// discover handler and open mqtt connection
 const { data, application } = require('ttn')
-// module.exports = {
-//   data: () => {
-//   },
-// }
+var fs = require("fs");
+
+var content = fs.readFileSync("currentData.json");
+console.log("Output Content : \n"+ content);
 
 const appID = "washingmachine_nas"
 const accessKey = "ttn-account-v2.c-h2jhoxpesYC_0ETfiniOc52VBPHX4BUEPoCbu8VJ4"
 const appEUI = '70B3D57ED002B1EE'
 
-data(appID, accessKey)
-  .then(function (client) {
+module.exports = async () => {
+  try{
+    const client = await data(appID, accessKey)
     client.on("uplink", function (devID, payload) {
       console.log("Received uplink from ", devID)
       console.log(payload)
+      fs.writeFile('currentData.json', JSON.stringify(payload), console.warn)
     })
-  })
-  .catch(function (err) {
+    client.on('activation', (devID, payload) => {
+      console.log("Received activation from ", devID)
+      console.log(payload)
+    })
+  } catch (err) {
     console.error(err)
     process.exit(1)
-  })
+  }
 
-// discover handler and open application manager client
-application(appID, accessKey)
-  .then(function (client) {
-    return client.get()
-  })
-  .then(function (app) {
-    console.log("Got app", app)
-  })
-  .catch(function (err) {
-    console.error(err)
-    process.exit(1)
-  })
+  // discover handler and open application manager client
+  application(appID, accessKey)
+    .then(function (client) {
+      return client.get()
+    })
+    .then(function (app) {
+      console.log("Got app", app)
+    })
+    .catch(function (err) {
+      console.error(err)
+      process.exit(1)
+    })
+}
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 var express = require('express');
 const mqtt = require('../mqtt');
-
 var router = express.Router();
+var fs = require("fs");
+mqtt()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(mqtt.data())
   res.render('index', { title: 'Express' });
 });
+
+
+router.get('/data', (req, res) => {
+  let content = fs.readFileSync("currentData.json");
+  content = JSON.parse(content)
+  res.send({content})
+})
 
 module.exports = router;
